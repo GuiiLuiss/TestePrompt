026ff16e8e9dc9807a51617de9eaac8a2a514c74
Guilherme Luís Silva <guilhermeluis79@hotmail.com>
2024-02-28 17:59:46 -0300

    Testando spell

--- check_spell.py
+++ check_spell.py
@@ -1,13 +1,33 @@
 from spellchecker import SpellChecker
 import glob
+import re
 import sys
 
+def preprocess_markdown(content):
+    # Remove formatações de negrito e itálico
+    content = re.sub(r'\*\*|\*', '', content)
+    
+    # Mantém apenas o texto de links markdown
+    content = re.sub(r'\[([^\]]+)\]\([^\)]+\)', r'\1', content)
+    
+    # Remove cabeçalhos Markdown, capturando múltiplos níveis
+    content = re.sub(r'^#+\s*', '', content, flags=re.MULTILINE)
+    
+    # Remove citações Markdown
+    content = re.sub(r'^>\s*', '', content, flags=re.MULTILINE)
+    
+    # Adicione mais regras conforme necessário
+    return content
+
+
 def check_spelling(file_path):
-    spell = SpellChecker(language='pt')  # Ajuste o idioma conforme necessário
+    spell = SpellChecker(language='pt')  # Define o idioma para Português
     misspelled_words = []
 
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
+        # Pré-processa o conteúdo para remover ou ajustar a sintaxe do Markdown
+        content = preprocess_markdown(content)
         words = content.split()
         misspelled = spell.unknown(words)
         for word in misspelled:
@@ -25,17 +45,17 @@ def main():
     if not files_to_check:
         files_to_check = glob.glob('./**/*.txt', recursive=True)
 
-    all_clear = True  # Flag para acompanhar se todos os arquivos estão livres de erros
+    all_clear = True
 
     for file_path in files_to_check:
         if not check_spelling(file_path):
             all_clear = False
 
     if not all_clear:
-        sys.exit(1)  # Sair com erro se alguma palavra incorreta for encontrada em qualquer arquivo
+        sys.exit(1)  # Sair com erro se palavras incorretas forem encontradas
     else:
         print("Nenhuma palavra incorreta encontrada em nenhum arquivo.")
-        sys.exit(0)  # Sair com sucesso se todas as palavras estiverem corretas
+        sys.exit(0)  # Sair com sucesso se estiver tudo correto
 
 if __name__ == "__main__":
     main()
